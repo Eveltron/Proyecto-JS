f689c23307a5fb178ef718316c25daec64faee35
Eveltron <miguelev028@gmail.com>
2022-06-12 12:23:23 +0000

    se añadio el boton de borrar a la tabla, no dañarlo porfavor XD

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ document.addEventListener('DOMContentLoaded',function() {
 	const table = document.getElementById('table');
 	const alert = document.getElementById('alert');
 	const btn = document.getElementById('add');
+	let id = 1;
 
+	function removeTodo(id){
+		console.log(id);
+		document.getElementById(id).remove();
+
+	}
 	
-	
-	function addtodo(){
+	function addTodo(){
 		if (title.value === "" || description.value === ""){
 			alert.classList.remove("d-none");
 			alert.innerText = "title and description are required";
 			return;
 		}
-		alert.classList.add("d-none");
+		alert.classList.add('d-none');
 		const row = table.insertRow();
+		row.setAttribute('id', id++);
 		row.innerHTML = `
 		<td>${title.value}</td>
 		<td>${description.value}</td>
@@ -25,12 +31,17 @@ document.addEventListener('DOMContentLoaded',function() {
                 <button class="btn btn-primary mb-1">
                   <i class="fa fa-pencil"></i>
                 </button>
-                <button class="btn btn-danger mb-1 ml-1">
-                  <i class="fa fa-trash"></i>
-                </button>
               </td>
 		`;
+		const removeBtn = document.createElement('button');
+		removeBtn.classList.add('btn', 'btn-danger','mb-1','ml-1' 	);
+		removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
+		removeBtn.onclick = function (e) {
+			removeTodo(row.getAttribute('id'));
+		}
+		row.children[3].appendChild(removeBtn);
+
 	}
 	
-	btn.onclick = addtodo;
+	btn.onclick = addTodo;	
 });
