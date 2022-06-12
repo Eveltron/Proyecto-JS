ce3f049cfdb1cf51323d95aba24c80710f444010
Eveltron <miguelev028@gmail.com>
2022-06-12 09:43:22 +0000

    se añadio una alerta y boton de titulo y description

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-document.addEventListener('DOMContentLoaded',function(){
-	const btn = document.getElementById("add");
-})
-const btn = document.getElementById('add');
+document.addEventListener('DOMContentLoaded',function() {
+	const title = document.getElementById('title');
+	const description = document.getElementById('description');
+	const table = document.getElementById('table');
+	const btn = document.getElementById('add');
 
-
+	
+	
+	function addtodo(){
+		if (title.value === "" || description.value === ""){
+			console.error("title and description are required");	
+		}else{
+			console.log("ok");
+		}
+	}
+	
+	btn.onclick = addtodo;
+});
